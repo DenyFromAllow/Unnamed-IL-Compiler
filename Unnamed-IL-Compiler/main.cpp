d900ca101b86ceec87137bffaf22acd28df21604
DenyFromAllow <1991927138@qq.com>
2016-07-17 18:47:06 +0800

    Hello,World!

--- Unnamed-IL-Compiler/main.cpp
+++ Unnamed-IL-Compiler/main.cpp
@@ -1,5 +1,21 @@
 #include"Simple\simple.h"
+using namespace Simple;
 
 int main() {
+	String HelloWorld;
+	HelloWorld << 'H';
+	HelloWorld << 'e';
+	HelloWorld << 'l';
+	HelloWorld << 'l';
+	HelloWorld << 'o';
+	HelloWorld << ',';
+	HelloWorld << 'W';
+	HelloWorld << 'o';
+	HelloWorld << 'r';
+	HelloWorld << 'l';
+	HelloWorld << 'd';
+	HelloWorld << '!';
+	HelloWorld++;
+	Sleep(5_sec);
 	return 0;
 }
