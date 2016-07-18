3ceaf9fc86dfcf5762515a30b1fb6ffa6a02da4d
DenyFromAllow <1991927138@qq.com>
2016-07-18 14:44:29 +0800

    更新库

--- Unnamed-IL-Compiler/Simple/TextProcessing/Dictionary.h
+++ Unnamed-IL-Compiler/Simple/TextProcessing/Dictionary.h
@@ -4,43 +4,15 @@ class SIMPLE_TRIETREE {
 protected:
 	class TrieNode {
 	public:
-		uint32 String;
-		TrieNode* Child[256];
-		TrieNode() {
-			//printf("bbb");
-			for(uint16 i = 0; i < 256; i++)Child[i] = nullptr;
-			String = 0;
-		}
+		TrieNode* Child[4] = { nullptr,nullptr,nullptr,nullptr };
+		uint32 String = 0;
+
 	};
 	TrieNode root;
+	void Print(TrieNode* node, SIMPLE_STRING str, uint8 bitidx, char otptchar, uint8 idx);
 public:
-	/*
-	SIMPLE_TRIETREE() :root() {
-		printf("aaa");
-		root = TrieNode();
-	}*/
-	void add(const char*str,uint32 num) {
-		TrieNode* node = &root;
-		for(; *str != 0; str++) {
-			if(!node->Child[(uint8)*str]) {
-				node->Child[(uint8) *str] = new TrieNode;
-				node = node->Child[(uint8) *str];
-			} else {
-				node = node->Child[(uint8) *str];
-			}
-		}
-		if(!node->String)node->String = num;
-	}
-	uint32 find(const char*str) {
-		TrieNode* node = &root;
-		for(; *str != 0; str++) {
-			if(!node->Child[(uint8) *str]) {
-				return 0;
-			} else {
-				node = node->Child[(uint8) *str];
-			}
-		}
-		return node->String;
-	}
+	void add(const char*str, uint32 num);
+	uint32 find(const char*str);
+	void PrintAll();
 
 };
--- Unnamed-IL-Compiler/main.cpp
+++ Unnamed-IL-Compiler/main.cpp
@@ -1,7 +1,19 @@
+#include <iostream>
 #include"Simple\simple.h"
 using namespace Simple;
+using namespace std;
 
 int main() {
+	SIMPLE_TRIETREE trie;
+	trie.add("asd", 1);
+	trie.add("dsa", 2);
+	trie.add("c++", 3);
+	trie.add("整数", 4);
+
+	cout << trie.find("c++");
+	cout << trie.find("整数");
+	trie.PrintAll();
+
 	String HelloWorld;
 	HelloWorld << 'H';
 	HelloWorld << 'e';
